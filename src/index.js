c3a042977b367d7e6de1c41d555deec373438071
GauravMarathe <gauravmarathe6468@gmail.com>
2023-12-22 20:31:51 +0530

    simple greet js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+let currDate = new Date(2023, 12, 22, 15);
+currDate = currDate.getHours();
+let greeting = '';
+
+
+if (currDate >= 1 && currDate < 12) {
+  greeting = 'Good Morning';
+} else if (currDate >= 12 && currDate < 19) {
+  greeting = 'Good Afternoon';
+  } else {
+    greeting = 'Good Night';
+  }
+
+ReactDOM.render(
+  
+    <h1>Hello Sir { greeting }</h1>,
+  document.getElementById('root')
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
