ea56b740c7ef61c157dbdf93dcc7b74606f65eae
GauravMarathe <gauravmarathe6468@gmail.com>
2023-12-23 12:29:41 +0530

    add css

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 
 let currDate = new Date(2023, 12, 22, 15);
 currDate = currDate.getHours();
 let greeting = '';
+const cssStyle = {};
 
 
 if (currDate >= 1 && currDate < 12) {
   greeting = 'Good Morning';
+  cssStyle.color = 'green';
 } else if (currDate >= 12 && currDate < 19) {
   greeting = 'Good Afternoon';
+  cssStyle.color = 'orange';
   } else {
     greeting = 'Good Night';
+    cssStyle.color = 'black';
   }
 
 ReactDOM.render(
+
+
+  <div>
   
-    <h1>Hello Sir { greeting }</h1>,
+
+    <h1>
+
+      Hello Sir, <span style={cssStyle}>  { greeting } </span>  
+      
+    </h1>
+  </div>,
   document.getElementById('root')
 
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pass a 
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 );
